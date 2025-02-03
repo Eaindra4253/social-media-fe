97c89546aea1dbde7ef1951e71640b7072813cf5
Ko Ko Lin Oo <kokolin.oo@cdsg.com.mm>
2025-02-03 15:34:15 +0630

    vite issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,9 @@ export default defineConfig({
   },
   preview: {
     port: 3000,
+    host: true,
     strictPort: true,
+    allowedHosts: true,
   },
   plugins: [react()],
   resolve: {
