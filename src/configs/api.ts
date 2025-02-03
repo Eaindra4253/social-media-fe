d5670d3db7b75ed318ea991396ef05778a505b37
Ko Ko Lin Oo <kokolin.oo@cdsg.com.mm>
2025-02-03 09:28:53 +0630

    update env

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {},
 });
 
