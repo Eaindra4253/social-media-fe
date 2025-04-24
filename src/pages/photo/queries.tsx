9e3cc8506802a0fe2b3f7894775b55f39c7c9bc1
Ko Ko Lin Oo <kokolin.oo@cdsg.com.mm>
2025-04-24 14:53:41 +0630

    image filter

--- src/pages/photo/queries.tsx
+++ src/pages/photo/queries.tsx
@@ -1,15 +1,18 @@
 import { photoKeys } from "@/configs/queryKeys";
 import { useParamsHelper } from "@/hooks/useParamHelper";
-import { getPhotos, uploadPhoto, deletePhoto } from "@/services/photo.service";
+import { deletePhoto, getPhotos, uploadPhoto } from "@/services/photo.service";
+import { useAuthStore } from "@/stores/auth.store";
 import { notifications } from "@mantine/notifications";
 import { IconCheck } from "@tabler/icons-react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useGetPhotos() {
+  const user = useAuthStore((state) => state.user);
   const { getParam } = useParamsHelper();
-  
+
   const params = {
     page: getParam("page") ?? 1,
+    outletType: user?.outletType ?? getParam("outletType") ?? undefined,
     limit: getParam("limit") ?? 10,
     type: getParam("type") ?? undefined,
   };
